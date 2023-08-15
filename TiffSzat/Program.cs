59ecf132eb6eacfebc21e76175dd4b3fed8fa384
Egor <Examle@gmail.com>
2023-08-15 14:58:19 +0700

    Сжмает все файлы формата .tiff в папке

--- TiffSzat/Program.cs
+++ TiffSzat/Program.cs
@@ -6,55 +6,65 @@ namespace TiffSzat
     {
         static void Main()
         {
-            Console.WriteLine("Перенесите файл в папку и нажмите ENTER");
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string fileSearchPattern = "*.tif";
+
+            try
+            {
+                string[] files = Directory.GetFiles(currentDirectory, fileSearchPattern);
+
+                foreach (string inputFile in files)
+                {
+                    CompressTiff(inputFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+
+            Console.WriteLine("Сжатие завершено. Нажмите ENTER.");
             Console.ReadLine();
-            Console.WriteLine("Введите имя файла:");
-            var readFile = Console.ReadLine();
-            string inputFilePath = readFile + ".tif";
-            var outputFile = readFile + "_сжат";
-            string outputFilePath = outputFile + ".tif";
+        }
+
+        static void CompressTiff(string inputFilePath)
+        {
+            string outputFilePath = Path.Combine(
+                Path.GetDirectoryName(inputFilePath) ?? throw new InvalidOperationException(),
+                Path.GetFileNameWithoutExtension(inputFilePath) + "_сжат.tif");
 
             using (Tiff inputTiff = Tiff.Open(inputFilePath, "r"))
+            using (Tiff outputTiff = Tiff.Open(outputFilePath, "w"))
             {
-                if (inputTiff == null)
+                if (inputTiff == null || outputTiff == null)
                 {
-                    Console.WriteLine("Failed to open input TIFF file.");
+                    Console.WriteLine($"Не удалось обработать файл: {inputFilePath}");
                     return;
                 }
 
-                using (Tiff outputTiff = Tiff.Open(outputFilePath, "w"))
+                int width = inputTiff.GetField(TiffTag.IMAGEWIDTH)[0].ToInt();
+                int height = inputTiff.GetField(TiffTag.IMAGELENGTH)[0].ToInt();
+                int bitsPerPixel = inputTiff.GetField(TiffTag.BITSPERSAMPLE)[0].ToInt();
+                int samplesPerPixel = inputTiff.GetField(TiffTag.SAMPLESPERPIXEL)[0].ToInt();
+
+                outputTiff.SetField(TiffTag.IMAGEWIDTH, width);
+                outputTiff.SetField(TiffTag.IMAGELENGTH, height);
+                outputTiff.SetField(TiffTag.BITSPERSAMPLE, bitsPerPixel);
+                outputTiff.SetField(TiffTag.SAMPLESPERPIXEL, samplesPerPixel);
+                outputTiff.SetField(TiffTag.COMPRESSION, Compression.LZW); // Задайте желаемый метод сжатия
+                outputTiff.SetField(TiffTag.PHOTOMETRIC, Photometric.MINISBLACK);
+                outputTiff.SetField(TiffTag.COMPRESSION, Compression.LZW);
+                outputTiff.SetField(TiffTag.PHOTOMETRIC, Photometric.MINISBLACK);
+
+                byte[] buffer = new byte[width * samplesPerPixel];
+                for (int i = 0; i < height; i++)
                 {
-                    if (outputTiff == null)
-                    {
-                        Console.WriteLine("Failed to create output TIFF file.");
-                        return;
-                    }
-
-                    // Копирование заголовков изображения
-                    int width = inputTiff.GetField(TiffTag.IMAGEWIDTH)[0].ToInt();
-                    int height = inputTiff.GetField(TiffTag.IMAGELENGTH)[0].ToInt();
-                    int bitsPerPixel = inputTiff.GetField(TiffTag.BITSPERSAMPLE)[0].ToInt();
-                    int samplesPerPixel = inputTiff.GetField(TiffTag.SAMPLESPERPIXEL)[0].ToInt();
-
-                    outputTiff.SetField(TiffTag.IMAGEWIDTH, width);
-                    outputTiff.SetField(TiffTag.IMAGELENGTH, height);
-                    outputTiff.SetField(TiffTag.BITSPERSAMPLE, bitsPerPixel);
-                    outputTiff.SetField(TiffTag.SAMPLESPERPIXEL, samplesPerPixel);
-                    outputTiff.SetField(TiffTag.COMPRESSION, Compression.LZW); // Задайте желаемый метод сжатия
-                    outputTiff.SetField(TiffTag.PHOTOMETRIC, Photometric.MINISBLACK);
-
-                    // Чтение и запись данных изображения
-                    byte[] buffer = new byte[width * samplesPerPixel];
-                    for (int i = 0; i < height; i++)
-                    {
-                        inputTiff.ReadScanline(buffer, i);
-                        outputTiff.WriteScanline(buffer, i);
-                    }
+                    inputTiff.ReadScanline(buffer, i);
+                    outputTiff.WriteScanline(buffer, i);
                 }
-            }
 
-            Console.WriteLine("Сжатие завержено. Нажмите ENTER.");
-            Console.ReadLine();
+                Console.WriteLine($"Файл сжат и сохранен: {outputFilePath}");
+            }
         }
     }
 }
